03d7449fc7e3fb8ecb2bbbc3591321c562a89ce1
Jeeyoung Kim <68314321+rlawldud53@users.noreply.github.com>
2024-08-26 11:05:52 +0900

    Update inference.sh

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=2 python ./inference.py --config ./configs/inference/inference_pose2img.yaml \
+CUDA_VISIBLE_DEVICES=0 python ./inference.py --config ./configs/inference/inference_pose2img.yaml \
                                            --ref_path ./test_imgs/reference/leverkusen_000013_000019.png\
                                            --seg_path ./test_imgs/seg_map/munster_000038_000019.png \
                                            --output_dir ./outputs \
